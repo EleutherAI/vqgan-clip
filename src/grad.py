ac2e6f66a3d01d0beaf32271a881352c6efaabbb
Stella Biderman <stellabiderman@gmail.com>
2021-10-26 21:32:14 -0400

    Create grad.py

---
+++ src/grad.py
@@ -0,0 +1,28 @@
+import torch
+
+class ReplaceGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x_forward, x_backward):
+        ctx.shape = x_backward.shape
+        return x_forward
+
+    @staticmethod
+    def backward(ctx, grad_in):
+        return None, grad_in.sum_to_size(ctx.shape)
+      
+replace_grad = ReplaceGrad.apply
+
+class ClampWithGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, min, max):
+        ctx.min = min
+        ctx.max = max
+        ctx.save_for_backward(input)
+        return input.clamp(min, max)
+
+    @staticmethod
+    def backward(ctx, grad_in):
+        input, = ctx.saved_tensors
+        return grad_in * (grad_in * (input - input.clamp(ctx.min, ctx.max)) >= 0), None, None
+      
+clamp_with_grad = ClampWithGrad.apply
