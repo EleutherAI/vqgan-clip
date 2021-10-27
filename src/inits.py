e679a0f51e3f0a9a8243f2d312f16c1ef9e4cc99
Stella Biderman <stellabiderman@gmail.com>
2021-10-26 22:03:18 -0400

    Create inits.py

---
+++ src/inits.py
@@ -0,0 +1,29 @@
+from PIL import ImageFile, Image, PngImagePlugin, ImageChops
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def random_noise_image(w,h):
+    random_image = Image.fromarray(np.random.randint(0,255,(w,h,3),dtype=np.dtype('uint8')))
+    return random_image
+
+
+# create initial gradient image
+def gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+
+def gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
+    result = np.zeros((height, width, len(start_list)), dtype=float)
+
+    for i, (start, stop, is_horizontal) in enumerate(zip(start_list, stop_list, is_horizontal_list)):
+        result[:, :, i] = gradient_2d(start, stop, width, height, is_horizontal)
+
+    return result
+
+    
+def random_gradient_image(w,h):
+    array = gradient_3d(w, h, (0, 0, np.random.randint(0,255)), (np.random.randint(1,255), np.random.randint(2,255), np.random.randint(3,128)), (True, False, False))
+    random_image = Image.fromarray(np.uint8(array))
+    return random_image
