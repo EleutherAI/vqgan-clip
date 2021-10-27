5aa79a56d45783374e412094c01a4e91c981947b
Stella Biderman <stellabiderman@gmail.com>
2021-10-26 21:08:39 -0400

    Create helpers.py

---
+++ src/helpers.py
@@ -0,0 +1,11 @@
+import torch
+import math
+
+def sinc(x):
+    return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
+
+
+def lanczos(x, a):
+    cond = torch.logical_and(-a < x, x < a)
+    out = torch.where(cond, sinc(x) * sinc(x/a), x.new_zeros([]))
+    return out / out.sum()
