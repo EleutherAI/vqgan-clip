e3371785acc717f02d14345126dc2bc6fba1f0c8
Stella Biderman <stellabiderman@gmail.com>
2021-11-02 00:28:21 -0400

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -5,12 +5,80 @@ import math
 def sinc(x):
     return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
 
-
 def lanczos(x, a):
     cond = torch.logical_and(-a < x, x < a)
     out = torch.where(cond, sinc(x) * sinc(x/a), x.new_zeros([]))
     return out / out.sum()
 
+
+def one_sided_clip_loss(input, target, labels=None, logit_scale=100):
+    input_normed = F.normalize(input, dim=-1)
+    target_normed = F.normalize(target, dim=-1)
+    logits = input_normed @ target_normed.T * logit_scale
+    if labels is None:
+        labels = torch.arange(len(input), device=logits.device)
+    return F.cross_entropy(logits, labels)
+
+
+class TVLoss(nn.Module):
+    def forward(self, input):
+        input = F.pad(input, (0, 1, 0, 1), 'replicate')
+        x_diff = input[..., :-1, 1:] - input[..., :-1, :-1]
+        y_diff = input[..., 1:, :-1] - input[..., :-1, :-1]
+        diff = x_diff**2 + y_diff**2 + 1e-8
+        return diff.mean(dim=1).sqrt().mean()
+
+    
+
+class GaussianBlur2d(nn.Module):
+    def __init__(self, sigma, window=0, mode='reflect', value=0):
+        super().__init__()
+        self.mode = mode
+        self.value = value
+        if not window:
+            window = max(math.ceil((sigma * 6 + 1) / 2) * 2 - 1, 3)
+        if sigma:
+            kernel = torch.exp(-(torch.arange(window) - window // 2)**2 / 2 / sigma**2)
+            kernel /= kernel.sum()
+        else:
+            kernel = torch.ones([1])
+        self.register_buffer('kernel', kernel)
+
+    def forward(self, input):
+        n, c, h, w = input.shape
+        input = input.view([n * c, 1, h, w])
+        start_pad = (self.kernel.shape[0] - 1) // 2
+        end_pad = self.kernel.shape[0] // 2
+        input = F.pad(input, (start_pad, end_pad, start_pad, end_pad), self.mode, self.value)
+        input = F.conv2d(input, self.kernel[None, None, None, :])
+        input = F.conv2d(input, self.kernel[None, None, :, None])
+        return input.view([n, c, h, w])
+
+class EMATensor(nn.Module):
+    def __init__(self, tensor, decay):
+        super().__init__()
+        self.tensor = nn.Parameter(tensor)
+        self.register_buffer('biased', torch.zeros_like(tensor))
+        self.register_buffer('average', torch.zeros_like(tensor))
+        self.decay = decay
+        self.register_buffer('accum', torch.tensor(1.))
+        self.update()
+    
+    @torch.no_grad()
+    def update(self):
+        if not self.training:
+            raise RuntimeError('update() should only be called during training')
+
+        self.accum *= self.decay
+        self.biased.mul_(self.decay)
+        self.biased.add_((1 - self.decay) * self.tensor)
+        self.average.copy_(self.biased)
+        self.average.div_(1 - self.accum)
+
+    def forward(self):
+        if self.training:
+            return self.tensor
+        return self.average
 
 def ramp(ratio, width):
     n = math.ceil(width / ratio + 1)
