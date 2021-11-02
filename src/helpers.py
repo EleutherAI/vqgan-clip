8d40db4202c309b77d08381ac0ce26c6ce8f8331
Stella Biderman <stellabiderman@gmail.com>
2021-11-02 00:26:50 -0400

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -22,6 +22,27 @@ def ramp(ratio, width):
     return torch.cat([-out[1:].flip([0]), out])[1:-1]
 
 
+def resample(input, size, align_corners=True):
+    n, c, h, w = input.shape
+    dh, dw = size
+
+    input = input.view([n * c, 1, h, w])
+
+    if dh < h:
+        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
+        pad_h = (kernel_h.shape[0] - 1) // 2
+        input = F.pad(input, (0, 0, pad_h, pad_h), 'reflect')
+        input = F.conv2d(input, kernel_h[None, None, :, None])
+
+    if dw < w:
+        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
+        pad_w = (kernel_w.shape[0] - 1) // 2
+        input = F.pad(input, (pad_w, pad_w, 0, 0), 'reflect')
+        input = F.conv2d(input, kernel_w[None, None, None, :])
+
+    input = input.view([n, c, h, w])
+    return F.interpolate(input, size, mode='bicubic', align_corners=align_corners)
+
 
 def spherical_dist(x, y, noise = False, noise_coeff=0.1):
     x_normed = F.normalize(x, dim=-1)
@@ -81,3 +102,26 @@ def resize_image(image, out_size):
     area = min(image.size[0] * image.size[1], out_size[0] * out_size[1])
     size = round((area * ratio)**0.5), round((area / ratio)**0.5)
     return image.resize(size, Image.LANCZOS)
+
+
+def size_to_fit(size, max_dim, scale_up=False):
+    w, h = size
+    if not scale_up and max(h, w) <= max_dim:
+        return w, h
+    new_w, new_h = max_dim, max_dim
+    if h > w:
+        new_w = round(max_dim * w / h)
+    else:
+        new_h = round(max_dim * h / w)
+    return new_w, new_h
+
+
+def fetch(url_or_path):
+    if str(url_or_path).startswith('http://') or str(url_or_path).startswith('https://'):
+        r = requests.get(url_or_path)
+        r.raise_for_status()
+        fd = io.BytesIO()
+        fd.write(r.content)
+        fd.seek(0)
+        return fd
+    return open(url_or_path, 'rb')
